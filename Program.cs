7a1a6cb47b25a29426bb1b4fe990163676370df5
Ekaterinabbl <kate4ka3007b@gmail.com>
2025-03-04 23:36:39 +0700

    обновление
    добавлен новый метод

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_ВОТ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public void Method()
+        {
+            Console.WriteLine("Ноасый метоод!");
+        }
+    }
+}
